a4c5f4b0e4e4819b92ace625aa0adf6375abd206
GabrielSaganic <34100192+GabrielSaganic@users.noreply.github.com>
2017-11-29 16:14:54 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>  
+        <link rel="stylesheet" type="text/css" href="CSS.css">
+        <title>Rate famous people</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <style>
+            body
+            {
+                background-repeat: no-repeat;
+                background-position: center center; 
+                background-size: cover;
+            }
+        </style>
+    </head>
+    <body background="pozadina.jpg">
+        <nav class="navbar sticky-top navbar-light bg-light">          
+            <a class="navbar-brand" href="index.php"><h1 class="font-weight-light">Rate Famous People</h1></a>
+            <a class="navbar-brand" href="Actor.php">Actor</a>
+            <a class="navbar-brand" href="Singer.php">Musicians</a>
+            <a class="navbar-brand" href="Politic.php">Politicians</a>
+            <a class="navbar-brand" href="AllRatings.php">All ratings</a>
+        </nav>
+        
+        <div class="drugi">
+            <img src="slika1.jpg" alt=""/>
+        </div>
+        </br>
+        <div class="text-center">
+            <p class="font-weight-light"><font size="5">Welcome to our rating site. Here you can rate your favorite or least favorite actor, musician or even
+                politician.
+            </br>
+            See what majority thinks and if you are one of them! We hope you will like the results :)</font></p>
+        </div>
+        <div class="text-center">         
+            <a class="badge badge-secondary" href="Actor.php"><h1>RATE US</h1></a>
+        </div>
+        </br>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    </body>
+    <footer class="footer">  
+        Created by: Dario Ognjanovic & Gabriel Saganic
+        </br>
+        Contact information: <a href="mailto:dognjanovic@student.uniri.hr">
+        dognjanovic@student.uniri.hr</a>
+       
+    </footer>
+</html>
+
